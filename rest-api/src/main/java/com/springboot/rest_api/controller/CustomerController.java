9fc729e05d23e62a8d5786ccdcfb6ee894fb6cc8
DhineshKumar <dhineshgokul03@gmail.com>
2025-03-27 14:49:13 +0530

    Rest Api Implementation

---
+++ rest-api/src/main/java/com/springboot/rest_api/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.springboot.rest_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.rest_api.model.Customer;
+import com.springboot.rest_api.service.CustomerService;
+
+@RestController
+public class CustomerController
+{
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/api/customer/hello")//Just for get or show the details
+	public String sayHello()
+	{
+		return "Hello from the First api in Springboot !!!!!!!";
+	}
+	
+	@PostMapping("/api/customer/add")//it is to send the details to db which are come from postman via this api
+	public Customer addCustomer(@RequestBody Customer customer)//this @requestbody is to get the request and give it to the Customer Model
+	{
+		//From this method we can able to read the data given by end-user
+//		System.out.println(customer.getName());
+//		System.out.println(customer.getContact());
+		return customerService.addCustomer(customer);
+	}
+	
+	@GetMapping("/api/customer/getall")
+	public List<Customer> getAllCustomer()
+	{
+		return customerService.getAllCustomer();
+	}
+
+}
---
+++ rest-api/src/main/java/com/springboot/rest_api/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.rest_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.rest_api.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>
+{
+	//save() method is the prebuild method in jpa
+	//findAll() is to fetch all the details in the db
+}
