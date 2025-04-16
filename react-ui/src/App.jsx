c1f29d0ac4ab05317f5b3373dfb753c04e4b25f3
DhineshKumar <dhineshgokul03@gmail.com>
2025-04-16 16:02:14 +0530

    working with Objects for Button blocks

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
 
 function App()
@@ -17,26 +17,74 @@ function App()
 // {
 //    setMsg("I have been Clicked by you and you generated me !!!!")
 // }
+  // let [name,setName] = useState(null)
+  // let [email,setEmail] = useState(null)
+  // let [contact,setContact] = useState(null)
 
+  // const populate = ()=>
+  // {
+  //   setName("Johnathon")
+  //   setEmail("johnaa@gmail.com")
+  //   setContact("9632587410")
+  // }
+  // const manager = ()=>
+  //   {
+  //     setName("Emma")
+  //     setEmail("emma@gmail.com")
+  //     setContact("6359635696")
+  //   }
 
-  let [name,setName] = useState(null)
-  let [email,setEmail] = useState(null)
-  let [contact,setContact] = useState(null)
-
-  const populate = ()=>
-  {
-    setName("Johnathon")
-    setEmail("johnaa@gmail.com")
-    setContact("9632587410")
-  }
-  const manager = ()=>
-    {
-      setName("Emma")
-      setEmail("emma@gmail.com")
-      setContact("6359635696")
-    }
+  //Creation of object and store the hardcoded values to it
+  const [employeeObj,setEmployeeObj] = useState({});
+  const [managerObj,setManagerObj] = useState({});
+  const [showEmp,setShowEmp] = useState(false); 
+   const [showManager,setShowManager] = useState(false);
   
+    useEffect (() =>
+    {
+      // this contains the empObject and ManagerObject with harcoded values
+      const populate = ()=>
+        {
+          let empObj = {
+            "name":"Laura Williams",
+            "email" : "emp@gmail.com",
+            "contact" : "8569321472",
+            "projectInfo" : "Web Design with Some of the internal projects"
+          }
 
+          let managerObj = {
+            "name":"Prenash",
+            "email" : "manager@gmail.com",
+            "contact" : "91234567898",
+            "departmentInfo" : "IT Department",
+            "employeeInfo": "Manages 45 employees"
+          }
+          /** Assign this local objects to states, so that we can display 
+       * states in other functions */
+            setEmployeeObj(empObj)
+            setManagerObj(managerObj)
+    
+        }
+        populate()
+    
+    },[]);
+
+    const populate = (val)=>{
+      switch(val){
+        case 'employee':
+          setShowEmp(true)  
+          setShowManager(false)
+          break; 
+        case 'manager':
+          setShowEmp(false)  
+          setShowManager(true)
+          break; 
+        default:
+          setShowEmp(false)  
+          setShowManager(false)
+          break; 
+      }
+    }
   return(
     <div>
     {/* <p>
@@ -46,7 +94,6 @@ function App()
         :`You are not cleared the exam because your percentage is ${percentage}%`
       }
     </p>
-
       <hr />
       <p>
         {
@@ -57,14 +104,47 @@ function App()
       </p>
       <hr /> */}
 
-      <button onClick={()=> {populate()} }>Click here to see the Details of the Employee </button>
-      <button onClick={()=> {manager()} }>Click here to see the Details of the Manager </button>
-      <p>
+      {/* <button onClick={()=> {populate()} }>Click here to see the Details of the Employee </button>
+      <button onClick={()=> {manager()} }>Click here to see the Details of the Manager </button> */}
+      {/* <p>
         Name : {name} <br />
         E-Mail : {email} <br />
         Contact : {contact} <br />
-      </p>
+      </p> */}
 
+          <button onClick={()=>{ populate('employee') }}>Populate Employee</button> 
+          <button onClick={()=>{ populate('manager') }}>Populate Manager</button>
+        {
+          showEmp === false? "" : 
+          <div id='employeeContent'>
+          <h3>Employee Block</h3>
+            <div>
+              Name: {employeeObj.name} <br />
+              Email: {employeeObj.email} <br />
+              Contact: {employeeObj.contact} <br />
+              <br />
+              <h4>Project Info</h4>
+              {employeeObj.projectInfo}
+            </div>
+         </div>
+        }
+       
+        {
+          showManager === false? "": 
+          <div id='managerContent'>
+          <h3>Manager Block</h3>
+           <div>
+             Name: {managerObj.name} <br />
+             Email: {managerObj.email} <br />
+             Contact: {managerObj.contact} <br />
+             <br />
+             <h4>Department Info</h4>
+             <span>{managerObj.departmentInfo} </span> 
+             <h4>All Employee Info</h4>
+             <span> {managerObj.employeeInfo}</span>
+           </div>
+          </div>
+        }
     </div>
   )
 }
