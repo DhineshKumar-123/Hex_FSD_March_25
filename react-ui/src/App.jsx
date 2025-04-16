f39653cb478065e3a626ba33ab5aafcfc01893ed
DhineshKumar <dhineshgokul03@gmail.com>
2025-04-16 13:02:53 +0530

    Set Keyword with usestate another example for populate attributes

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,44 +1,38 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 
 function App()
 {//App is a component and this must have a return 
 
-  let [name] = useState('John Doe');
-  // if we want to set the differnet values for the declared variables within a fuction we need to use usestate and setname or set keyword
+//   let [name] = useState('John Doe');
+//   // if we want to set the differnet values for the declared variables within a fuction we need to use usestate and setname or set keyword
+//   let[msg,setMsg] = useState('');
+//   const age = 50
+// //let can be updated when we need to modify
+// //const cant be altered or modified once it declared with a value
+// let percentage = 100
 
-  let[msg,setMsg] = useState('');
+// //create a function for clicking button
+// const btnclick = ()=>
+// {
+//    setMsg("I have been Clicked by you and you generated me !!!!")
+// }
 
-  const age = 50
 
-//let can be updated when we need to modify
-//const cant be altered or modified once it declared with a value
+  let [name,setName] = useState(null)
+  let [email,setEmail] = useState(null)
+  let [contact,setContact] = useState(null)
 
-let x = 20
-let y = 50
-
-let percentage = 100
-
-//create a function for clicking button
-const btnclick = ()=>
-{
-  // alert("You Clicked Me I am Here to assist you!!!!")
-  // console.log("You clicked me I am Here in the Console")
-
-  setMsg("I have been Clicked by you and you generated me !!!!")
-}
-
+  const populate = ()=>
+  {
+    setName("Johnathon")
+    setEmail("johnaa@gmail.com")
+    setContact("9632587410")
+  }
 
   return(
     <div>
-      <h1>React App Vite</h1>
-      <p>Name is {name}</p>
-      <p>Age is {age}</p>
-      <p>Multiplication is {x*y}</p>
-
-<hr />
-
-    <p>
+    {/* <p>
       {
         percentage>75?`You have cleared the Exam with ${percentage}%`
         :percentage>50?`You have an Re-Exam because you have scored ${percentage}%`
@@ -54,10 +48,14 @@ const btnclick = ()=>
           :`According to your age you are a Teenager`
         }
       </p>
-      <hr />
+      <hr /> */}
 
-      <button onClick={()=>btnclick()}>Click here to see the Different </button>
-      <p>{msg}</p>
+      <button onClick={()=> {populate()} }>Click here to see the Details of the below attributes </button>
+      <p>
+        Name : {name} <br />
+        E-Mail : {email} <br />
+        Contact : {contact} <br />
+      </p>
 
     </div>
   )
