a90fff0688e96538913a27ca7ef533b3f15d7a2a
DhineshKumar <dhineshgokul03@gmail.com>
2025-04-17 13:26:38 +0530

    login added with some more validations

--- react-ui/src/components/auth/Login.jsx
+++ react-ui/src/components/auth/Login.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import users from "../../data/users";
+
 function Login ()
 {
+
+    const[username,setUsername] = useState(null);
+    const[password,setPassword] = useState(null);
+    const[msgUsername,setMsgUsername] = useState(null);
+    const[msgPassword,setMsgPassword] = useState(null);
+    const[userData,setUserData] = useState(users);
+    
+
+    const login = ()=>
+    {
+        let isCorrect = false; 
+
+        if(username === null || username === "" || username === undefined)
+            {
+            setMsgUsername("Please Enter Username")
+            return;
+            }
+            else{
+                setMsgUsername(null);
+            }
+        if(password === null || password === "" || password === undefined)
+            {
+            setMsgPassword("Please Enter Password")
+            return 
+            }
+            else
+            {
+                setMsgPassword(null);
+            }
+
+
+            //Checking the username and Password given by enduser with the created dummy data users file 
+            userData.forEach(u => {
+                if(u.username === username && u.password === password )
+                {
+                    alert("U are legit and your role is " + u.role);
+                    isCorrect(true)
+                }
+                if(isCorrect === false)
+                {
+                    setMsgUsername("Invalid Credentials")
+                }
+            });
+
+        // console.log('i am on line 17')
+        // alert('alright, u r ready to be tested!!!!')
+    }
+
+
     return(
         <div>
             <div className="container-fluid">
@@ -23,17 +75,35 @@ function Login ()
                                 Login
                             </div>
                             <div class="card-body">
+                            {
+                                    msgUsername === null ? "" : <div className="mb-4">
+                                        {msgUsername}
+                                    </div>
+
+                            }
+                             {
+                                    msgPassword === null ? "" : <div className="mb-4">
+                                        {msgPassword}
+                                    </div>
+                                }
                                 <div className="mb-4">
-                                    <label>Username </label>
-                                    <input type="text" className="form-control" />
+                                    <label > Username </label> {/*This e is called as event*/}
+                                    <input type="text" className="form-control" 
+                                    onChange={ ($event) => {setUsername($event.target.value); 
+                                    setMsgUsername(null) }} />
+
                                 </div>
                                 <div className="mb-4">
-                                    <label>Password </label>
-                                    <input type="password" className="form-control"/>
+                                    <label > Password </label>
+                                    <input type="password" className="form-control" 
+                                    onChange={ ($event) => {setPassword($event.target.value); 
+                                    setMsgPassword(null) }} />
                                 </div>
+
                                 <div className="mb-4">
                                 {/* mb-margin bottom */}
-                                <button type="button" class="btn btn-primary">Login</button>
+                                <button type="button" class="btn btn-primary" 
+                                onClick={() => {login()}}> Login </button>
                                 </div>
                             </div>
                             <div className="card-footer">
@@ -52,4 +122,5 @@ function Login ()
         </div>
     )
 }
+
 export default Login;
